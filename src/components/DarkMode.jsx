895c629b59229c9004c91d123947fc80065e294b
Sanjin Dumisic <codebatine@proton.me>
2024-03-17 00:23:41 +0100

    dark mode fixes :night_with_stars:

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 class DarkMode extends React.Component {
   state = {
-    isDarkMode: false,
+    isDarkMode:
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches,
   };
+
+  componentDidMount() {
+    if (this.state.isDarkMode) {
+      document.body.classList.add('dark-mode');
+    }
+  }
 
   toggleDarkMode = () => {
     const isDarkMode = document.body.classList.toggle('dark-mode');
